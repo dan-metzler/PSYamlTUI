3c882116d8668dc3f0109c63317c8a858ef4b357
dan-metzler <182840042+dan-metzler@users.noreply.github.com>
2026-03-01 15:38:26 -0600

    [remove] delete NewModule.psd1 and NewModule.psm1 files. Remove output folder from github. No need to store build artifacts there.

--- Output/NewModule/NewModule.psm1
+++
@@ -1,7 +0,0 @@
-﻿#Region '.\Public\Get-CatFact.ps1' -1
-
-function Get-CatFact {
-    $result = Invoke-RestMethod -Uri "https://catfact.ninja/fact"
-    return $result.fact
-}
-#EndRegion '.\Public\Get-CatFact.ps1' 5
