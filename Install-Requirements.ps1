1f971cc7dd07c0ca1b3c1ca0ff3db261c9b8a25e
dan-metzler <182840042+dan-metzler@users.noreply.github.com>
2026-03-01 21:08:33 -0600

    [update] enhance module installation script with version constraints

--- Install-Requirements.ps1
+++ Install-Requirements.ps1
@@ -1,10 +1,18 @@
-$modules = @("ModuleBuilder", "InvokeBuild")
-foreach ($m in $modules) {
+$requiredModules = @(
+    @{ ModuleName = 'ModuleBuilder'; MinimumVersion = '3.1.8'; MaximumVersion = '4.999.999' }
+    @{ ModuleName = 'Pester'; MinimumVersion = '3.4.0'; MaximumVersion = '5.999.999' }
+    @{ ModuleName = 'InvokeBuild'; MinimumVersion = '5.14.23'; MaximumVersion = '6.999.999' }
+    @{ ModuleName = 'platyPS'; MinimumVersion = '0.14.2'; MaximumVersion = '1.999.999' }
+)
+
+foreach ($moduleItem in $requiredModules) {
 
     try {
-        Get-InstalledModule -Name $m -ErrorAction stop
-    } catch {
-        Write-Host "Installing $m"
-        Install-Module -Name $m -Scope CurrentUser -Force
+        Get-InstalledModule -Name $($moduleItem.ModuleName) -MinimumVersion $($moduleItem.MinimumVersion) -MaximumVersion $($moduleItem.MaximumVersion) -ErrorAction Stop | Select-Object Name, Version
+    }
+    catch {
+        Write-Host "Installing $($moduleItem.ModuleName)"
+        Install-Module -Name $($moduleItem.ModuleName) -MinimumVersion $($moduleItem.MinimumVersion) -MaximumVersion $($moduleItem.MaximumVersion) -Scope CurrentUser -Force
     }
 }
+
