9fcafc1bb272126c319ce8e89ac04f812cf1f86e
dan-metzler <182840042+dan-metzler@users.noreply.github.com>
2026-03-16 21:02:21 -0500

    [update] change theme file extension from .json to .yaml in examples

--- Docs/Examples/MenuLaunchers/Intermediate-Example.ps1
+++ Docs/Examples/MenuLaunchers/Intermediate-Example.ps1
@@ -3,7 +3,7 @@ $repoRoot = Join-Path (Join-Path (Join-Path $here '..') '..') '..'
 $moduleManifest = Join-Path -Path $repoRoot -ChildPath 'Source\PSYamlTUI.psd1'
 $fixturesRoot = Join-Path (Join-Path $here '..') 'Fixtures'
 $menuPath = Join-Path -Path $fixturesRoot -ChildPath 'menus\intermediate.menu.yaml'
-$themePath = Join-Path -Path $fixturesRoot -ChildPath 'themes\intermediate.theme.json'
+$themePath = Join-Path -Path $fixturesRoot -ChildPath 'themes\intermediate.theme.yaml'
 
 Import-Module -Name $moduleManifest -Force
 
@@ -15,10 +15,4 @@ $statusData = @{
     'Environment'  = 'Production'
 }
 
-$themeObject = Get-Content -Path $themePath -Raw | ConvertFrom-Json
-$theme = @{}
-$themeObject.PSObject.Properties | ForEach-Object {
-    $theme[$_.Name] = [string]$_.Value
-}
-
-Start-Menu -Path $menuPath -StatusData $statusData -Theme $theme -BorderStyle Rounded
+Start-Menu -Path $menuPath -StatusData $statusData -ThemePath $themePath -BorderStyle Rounded
--- Docs/Examples/MenuLaunchers/Simple-Example.ps1
+++ Docs/Examples/MenuLaunchers/Simple-Example.ps1
@@ -3,14 +3,8 @@ $repoRoot = Join-Path (Join-Path (Join-Path $here '..') '..') '..'
 $moduleManifest = Join-Path -Path $repoRoot -ChildPath 'Source\PSYamlTUI.psd1'
 $fixturesRoot = Join-Path (Join-Path $here '..') 'Fixtures'
 $menuPath = Join-Path -Path $fixturesRoot -ChildPath 'menus\simple.menu.yaml'
-$themePath = Join-Path -Path $fixturesRoot -ChildPath 'themes\simple.theme.json'
+$themePath = Join-Path -Path $fixturesRoot -ChildPath 'themes\simple.theme.yaml'
 
 Import-Module -Name $moduleManifest -Force
 
-$themeObject = Get-Content -Path $themePath -Raw | ConvertFrom-Json
-$theme = @{}
-$themeObject.PSObject.Properties | ForEach-Object {
-    $theme[$_.Name] = [string]$_.Value
-}
-
-Start-Menu -Path $menuPath -Theme $theme -BorderStyle Single
+Start-Menu -Path $menuPath -ThemePath $themePath -BorderStyle Single
